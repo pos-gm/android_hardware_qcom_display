2ab50156042834162d93a14be6fefa5ca67f4b4b
danielml <daniel@danielml.dev>
2023-05-18 19:04:10 +0200

    sdm: hwc: Allow enabling doze mode support with a property
    * By default the HWC restricts the doze mode support
      to the dsi_cmd_mode panel type
    
    * This change allows enabling doze for devices that, with
      other panel types, still support it
    
    Change-Id: I50226dbbdd9dcdd947c7a13070eecdd2ec104aaf
    Signed-off-by: danielml <daniel@danielml.dev>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- include/display_properties.h
+++ include/display_properties.h
@@ -181,6 +181,7 @@
 #define DISABLE_IDLE_TIME_HDR                DISPLAY_PROP("disable_idle_time_hdr")
 #define DISABLE_IDLE_TIME_VIDEO              DISPLAY_PROP("disable_idle_time_video")
 #define DISABLE_CACHE_MANAGER                DISPLAY_PROP("disable_cache_manager")
+#define OVERRIDE_DOZE_MODE_PROP              DISPLAY_PROP("override_doze_mode")
 // Add all other.properties above
 // End of property
 #endif  // __DISPLAY_PROPERTIES_H__
